190a2c16e0683696dc4251c8a1de35540d80aac5
Ushiris_lab <ushiriscco@gmail.com>
2019-10-15 21:14:08 +0900

    update model

--- New Unity Project/Assets/Scripts/MainCamera.cs
+++ New Unity Project/Assets/Scripts/MainCamera.cs
@@ -9,28 +9,20 @@ public class MainCamera : MonoBehaviour
     public GameObject player;
     PlayerController playerController;
 
+    public chaser pivot;
+    public Vector3 diff=new Vector3(0, 2, -4);
+
     // Start is called before the first frame update
     void Start()
     {
         playerController = player.GetComponent<PlayerController>();
+        //プレイヤーと同一の座標へ向かい、そこから離れます。
+        transform.position = pivot.target.transform.position;
+        transform.Translate(diff, Space.Self);
     }
     
     void LateUpdate()
     {
-        //プレイヤーの方向と同一の方向を向き、補正をかけます。
-        transform.rotation =player.transform.rotation;
-        if (playerController.looking == LR.right)
-        {
-            transform.Rotate(new Vector3(0, 270, 0));
-        }
-        else
-        {
-            transform.Rotate(new Vector3(0, 90, 0));
-        }
-        
-        //プレイヤーと同一の座標へ向かい、そこから離れます。
-        transform.position = player.transform.position;
-        transform.Translate(0, 2, -4, Space.Self);
-        transform.LookAt(player.transform);
+        transform.LookAt(pivot.target.transform);
     }
 }
