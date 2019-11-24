c918f2baacd594ff6f2e7f31dcfe86a1ab60f28c
Unknown <umiushicco@gmail.com>
2019-11-25 06:37:52 +0900

    add sweets stage
    お菓子ステージのテストとして最低限の実装を行いました。残りは寝た後にやる。

---
+++ New Unity Project/Assets/Scripts/MemoryEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryEvent : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            target.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
