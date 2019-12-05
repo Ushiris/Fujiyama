b38ad3b42f72273f24fec2a7ac806ce7b4a722e5
Ushiris_lab <ushiriscco@gmail.com>
2019-12-05 13:05:29 +0900

    gyuyjhgbikh

---
+++ New Unity Project/Assets/resouces/デザイン分野/stage/お菓子/お菓子/Baumkuchen/roll_data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roll_data : MonoBehaviour
+{
+    public bool isStart = false;
+    public float top_r = 0;
+    public float bottom_r = 0;
+    public float height = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
